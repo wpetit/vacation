f191c944297e15f5685f2e8f5412ee6546402ea0
wpetit <petit.wilfried@gmail.com>
2014-06-15 19:02:45 +0200

    Update pagination

--- vacation-webapp/src/main/webapp/script/vacation/controller.js
+++ vacation-webapp/src/main/webapp/script/vacation/controller.js
@@ -1,7 +1,8 @@
 // Create a controller with name VacationsCtrl to bind to the html page.
 app.controller('VacationsCtrl', function($scope, vacationService) {
 	// Refresh the grid, calling the appropriate service method.
-	$scope.refreshGrid = function(page) {
+	$scope.pageChanged = function() {
+		var page = $scope.pagingOptions.currentPage;
 		vacationService.getAll(($scope.pagingOptions.pageSize * (page - 1)), 5)
 				.success(function(data) {
 					$scope.modelList = data.modelList;
@@ -12,7 +13,7 @@ app.controller('VacationsCtrl', function($scope, vacationService) {
 	$scope.pagingOptions = {
 		pageSize : 5,
 		currentPage : 1
-	}
+	};
 
 	$scope.gridOptions = {
 		data : 'modelList',
@@ -20,6 +21,5 @@ app.controller('VacationsCtrl', function($scope, vacationService) {
 		pagingOptions : $scope.pagingOptions
 	};
 
-	$scope.refreshGrid(1);
+	$scope.pageChanged();
 });
-
