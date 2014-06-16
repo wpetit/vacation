ae1427115d7e84f22099330b4dfcbe2f56d0b31d
wpetit <petit.wilfried@gmail.com>
2014-06-16 22:37:27 +0200

    Implements navbar and logout button + update routes

--- vacation-webapp/src/main/webapp/script/app.js
+++ vacation-webapp/src/main/webapp/script/app.js
@@ -1,18 +1,16 @@
-var app = angular
-		.module('vacationApp', [ 'ngRoute', 'ngGrid', 'ui.bootstrap', 'vacationAppControllers' ]);
+var app = angular.module('vacationApp', [ 'ngRoute', 'ngGrid', 'ui.bootstrap',
+		'vacationAppControllers' ]);
 
 var vacationAppControllers = angular.module('vacationAppControllers', []);
 
-
 app.config([ '$routeProvider', function($routeProvider) {
-	$routeProvider.when('/vacations', {
+	$routeProvider.when('/list', {
 		templateUrl : 'partials/vacation-list.html',
 		controller : 'VacationsCtrl'
 	}).when('/vacationTypes', {
 		templateUrl : 'partials/vacation-type-list.html',
 		controller : 'VacationTypeCtrl'
-	}).
-	otherwise({
-		redirectTo : '/vacations'
+	}).otherwise({
+		redirectTo : '/list'
 	});
 } ]);
