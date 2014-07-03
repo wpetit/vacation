7f4a2f669894dad75a2b128a24e0850a6c213171
wpetit <petit.wilfried@gmail.com>
2014-07-03 18:17:19 +0200

    Update style and form validation

--- vacation-webapp/src/main/webapp/script/filters/filters.js
+++ vacation-webapp/src/main/webapp/script/filters/filters.js
@@ -4,12 +4,12 @@ vacationAppFilters.filter('sortIconFilter', function() {
 		var icon = "";
 		if (sortOptions.field == input) {
 			if ("desc" == sortOptions.direction) {
-				icon = 'glyphicon glyphicon-sort-by-attributes-alt';
+				icon = 'glyphicon glyphicon-sort-by-attributes-alt sortIcon';
 			} else {
-				icon = 'glyphicon glyphicon-sort-by-attributes';
+				icon = 'glyphicon glyphicon-sort-by-attributes sortIcon';
 			}
 		} else {
-			icon = 'glyphicon glyphicon-sort';
+			icon = 'glyphicon glyphicon-sort sortIcon';
 		}
 		return icon;
 	};
